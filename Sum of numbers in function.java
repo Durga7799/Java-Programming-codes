531b0523905134294c6bff9b85e3216cc14fabaf
DURGA PRASAD REDDY DESAM <112537248+Durga7799@users.noreply.github.com>
2024-07-01 12:01:04 +0530

    Create Sum of numbers in function.java

---
+++ Sum of numbers in function.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Sum{
+    public static void main(String[] args) {
+        sum();
+    }
+        static void sum(){
+            Scanner in=new Scanner(System.in);
+            int a=in.nextInt();
+            int b=in.nextInt();
+            int c=a+b;
+             System.out.println("The sum of 2 numbers is " + c);
+        }
+}
