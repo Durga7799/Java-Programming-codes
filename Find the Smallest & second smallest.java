a676c680da73cc45753e38e2ed0319bb236761df
DURGA PRASAD REDDY DESAM <112537248+Durga7799@users.noreply.github.com>
2024-07-12 19:10:53 +1400

    Create  Find the Smallest & second smallest.java
    Write a program to find the smallest and second smallest elements present in an array, Consider no duplicates are present and you will be taking the array size and the elements of an array as input to the program.
    
    Sample Input
    5
    4
    7
    5
    3
    10
    
    Sample Output
    3
    4

---
+++ Find the Smallest & second smallest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int arr[]={5,4,7,5,3,10};
+      int n=arr.length;
+      Arrays.sort(arr);
+      System.out.println("The smallest element is " + arr[0]);
+      System.out.println("The second smallest element is " + arr[1]);
+  }
+}
