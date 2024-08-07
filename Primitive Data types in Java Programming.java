f34f8157dcbdb5c77235d7a436b6e6dd95283937
DURGA PRASAD REDDY DESAM <112537248+Durga7799@users.noreply.github.com>
2024-08-07 11:57:15 +0530

    Create Primitive Data types in Java Programming.java

---
+++ Primitive Data types in Java Programming.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        byte age=35;//Data type  for byte[-128,128],1 byte
+        int myage=30;//Data type  for Integer[-2B,2B],2 bytes
+        int herage=myage;
+        long num=123456789L;//Data type  for Long,8 bytes
+        float price=12.9F;//Data type  for Float,4 bytes
+        double rate=14.6D;//Data type  for double,8 bytes
+        char letter='a';//Data type  for Character,2 bytes
+        boolean isEligible=true;//Data type  for boolean,1 byte
+        System.out.println(age);
+         System.out.println(myage);
+          System.out.println(herage);
+           System.out.println(num);
+            System.out.println(price);
+             System.out.println(rate);
+              System.out.println(letter);
+               System.out.println(isEligible);
+    }
+}
